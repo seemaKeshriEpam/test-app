99d8fb50e2d91de81c37bbea6ad5b87aee63632f
seemaKeshriEpam <seema_keshri@epam.com>
2026-07-27 12:56:57 +0530

    Create tests/epam-client-work.spec.ts

---
+++ tests/epam-client-work.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('EPAM client work navigation', () => {
+  test('navigates from Services to Client Work', async ({ page }) => {
+  });
+});
